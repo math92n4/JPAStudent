bb0509313453f43de4a607e5c0282482f14882de
math92n4 <math92n4@stud.kea.dk>
2023-09-07 22:41:57 +0200

    ready for deployment

--- src/test/java/com/example/jpastudent/repo/StudentRepositoryTest.java
+++ src/test/java/com/example/jpastudent/repo/StudentRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.jpastudent.model.Student;
 import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
